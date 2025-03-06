f91a29a5e27790aa85362e5f518a19bd9211dd45
alasiri <austinalasiri44@gmail.com>
2025-03-06 12:24:35 +0300

    Redid the test

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-cats = ["Milo", "Otis", "Garfield"];
-
-function cats(){
-
-}
+const cats = ['Milo', 'Otis', 'Garfield'];
 
 function destructivelyAppendCat(name){
     cats.push(name);
@@ -12,26 +8,26 @@ function destructivelyPrependCat(name){
     cats.unshift(name);
 }
 
-function destructivelyRemoveLastCat(name){
-    cats.pop(name);
+function destructivelyRemoveLastCat(){
+    cats.pop();
 }
 
-function destructivelyRemoveFirstCat(name){
-    cats.shift(name);
+function destructivelyRemoveFirstCat(){
+    cats.shift();
 }
 
 function appendCat(name){
-    return [...cats,name];
+    return [...cats, name];
 }
 
 function prependCat(name){
-    return [name, ...cats];    
+    return [name, ...cats];
 }
 
 function removeLastCat(){
-    return cats.slice(0,-1);   
+    return cats.slice(0, cats.length - 1);
 }
 
 function removeFirstCat(){
-    return cats.slice(1);    
+    return cats.slice(1);
 }
